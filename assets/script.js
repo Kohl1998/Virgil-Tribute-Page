e4c02ffe65e359bf47ab3ff061c1c5f4863ddac0
kohlvbusiness98@gmail.com <kohlvbusiness98@gmail.com>
2022-12-28 14:37:11 +0000

    added alert, confirm, and prompt actions to file

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,12 @@
 // Add variables for Virgil's info
 // Console log info 
 
+alert("Welcome to my tribute page on Virgil Abloh for the believers")
+
+var feedback = confirm("If you wish to continue, click OK",)
+
+var appreciation = prompt("How was Virgil impacted your life?")
+
+alert(feedback)
+
+alert(appreciation)
